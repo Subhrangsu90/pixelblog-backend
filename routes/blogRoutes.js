2b62deb1a4c352e8c66d6b81d749545a3723a1df
Subhrangsu90 <subhrangsubera@outlook.com>
2024-03-31 23:58:39 +0530

    post,update,delete add

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -30,4 +30,56 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// POST a new blog
+router.post("/", async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: newBlog,
+      message: "Blog created successfully",
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
+// UPDATE a blog by ID
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedBlog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.json({
+      success: true,
+      data: updatedBlog,
+      message: "Blog updated successfully",
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
+// DELETE a blog by ID
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(id);
+    if (!deletedBlog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.json({
+      success: true,
+      data: deletedBlog,
+      message: "Blog deleted successfully",
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
 module.exports = router;
